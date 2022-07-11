0a50fa4ce6aa5c740e0dd54b94bb8d6a28bb087a
Ese Kenneth Mudiaga <kenesecorp@hotmail.com>
2022-07-12 00:42:17 +0100

    Add receipts to subscription

---
+++ TerraMart.Domain/Clients/Receipt.cs
@@ -0,0 +1,18 @@
+﻿namespace TerraMart.Domain.Clients;
+
+public class Receipt
+{
+    /// <summary>
+    /// Unique Id or number for this customer receipt
+    /// </summary>
+    public string ReceiptId { get; set; }
+    /// <summary>
+    /// Transaction reference
+    /// </summary>
+    public string TransactionId { get; set; }
+    public decimal Amount { get; set; }
+    public bool IsConfirmed { get; set; }
+    public DateTime Date { get; set; }
+    public string? ConfirmedBy { get; set; }
+    public DateTime? DateConfirmed { get; set; }
+}
--- TerraMart.Domain/Clients/Subscription.cs
+++ TerraMart.Domain/Clients/Subscription.cs
@@ -13,13 +13,22 @@ namespace TerraMart.Domain.Clients
         public string ProductId { get; set; }
         public ProductType ProductType { get; set; }
         public string ProductName { get; set; }
+        /// <summary>
+        /// Nominal price (face-value) of the property
+        /// </summary>
         public decimal Price { get; set; }
+        /// <summary>
+        /// Actual Amount to be paid (Price - Discounts)
+        /// </summary>
+        public decimal Amount { get; set; }
         public decimal? Area { get; set; }
         public DateTime DateCreated { get; set; }
         public PaymentPlan? Plan { get; set; }
         public bool IsApproved { get; set; }
         public DateTime? DateApproved { get; set; }
         public string? ApprovedBy { get; set; }
+        public Discount? Discount { get; set; }
+        public List<Receipt>? Receipts { get; set; }
 
         public DateTime? ExpectedCompletionDate => DateApproved?.AddMonths(Plan?.Duration ?? 0);
     }
